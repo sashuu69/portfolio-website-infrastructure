d200f2d282f61a14555941958198364ae0d90a3d
Sashwat K <sashwat0001@gmail.com>
2024-05-28 00:14:33 +0530

    feat: add ansible-playbook to bring up portfolio website

--- output.tf
+++ output.tf
@@ -1,3 +1,13 @@
+output "private_key" {
+  description = "Private key path"
+  value = replace(var.public_key_path, ".pub", "")
+}
+
+output "user" {
+  description = "Username of EC2 instance"
+  value = "ubuntu"
+}
+
 output "public_ip" {
   description = "Public IP of created instance"
   value       = aws_eip_association.portfolio_website_eip_association.public_ip
@@ -5,5 +15,5 @@ output "public_ip" {
 
 output "ssh-command" {
   description = "SSH command to connect to created EC2 instance"
-  value       = "ssh -i ${replace(var.public_key_path, ".pub", "")} ubuntu@${aws_eip_association.portfolio_website_eip_association.public_ip}"
+  value       = "ssh -i ${private_key} ${user}@${public_ip}"
 }
