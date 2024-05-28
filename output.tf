3e12c8f7961b4fd604b8d898132a3ea5e515bac0
Sashwat K <sashwat0001@gmail.com>
2024-05-28 19:31:20 +0530

    feat: add ansible playbook to setup application

--- output.tf
+++ output.tf
@@ -1,11 +1,6 @@
 output "private_key" {
   description = "Private key path"
-  value = replace(var.public_key_path, ".pub", "")
-}
-
-output "user" {
-  description = "Username of EC2 instance"
-  value = "ubuntu"
+  value = local.private_key_path
 }
 
 output "public_ip" {
@@ -15,5 +10,5 @@ output "public_ip" {
 
 output "ssh-command" {
   description = "SSH command to connect to created EC2 instance"
-  value       = "ssh -i ${private_key} ${user}@${public_ip}"
+  value       = "ssh -i ${local.private_key_path} ${var.instance_username}@${aws_eip_association.portfolio_website_eip_association.public_ip}"
 }
